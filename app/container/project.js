06ea03a8b30f74eb060bb0b751f33ce6e837ea48
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-09-05 23:03:18 +0200

    Second iteration of project page: added fullscreen picture, iteration on top bar.

--- app/container/project.js
+++ app/container/project.js
@@ -4,12 +4,33 @@
 
 var React = require("react");
 
-var TopBar = require("../view/topbar");
+var ReactRouter = require("react-router");
+var Link = ReactRouter.Link;
 
 var ProjectContainer = (props) => {
     return (
         <div className="container-root">
-            <TopBar/>
+            <div className="topbar-container">
+                <div className="topbar">
+                    <h3 className="topbar-title">Tom van Kruijsbergen</h3>
+                    <Link to="/" className="topbar-back">
+                        <div className="topbar-back-image">
+                            <img type="image/svg" src="images/arrow-left-white.svg"/>
+                        </div>
+                        <div className="topbar-back-text">Back</div>
+                    </Link>
+                </div>
+            </div>
+            <div className="container-topbar-offset">
+                <div className="widescreen-image">
+                    <div className="widescreen-image-image-container">
+                        <img className="widescreen-image-image" src="images/test1.jpg"/>
+                    </div>
+                    <div className="widescreen-image-text-container">
+                        <h1 className="widescreen-image-text">Kik Chatbot</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
